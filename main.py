24ee34701e0caa10044dcf8ffd0d899bdc27a5e1
Yarosya <rozvazkyi1999@gmail.com>
2024-08-15 19:50:03 +0300

    create,change,delete func for app

--- main.py
+++ main.py
@@ -25,6 +25,42 @@ def main():
             break
         elif command == "hello":
             print("How can I help you?")
+        elif command == "add":
+            print(vm.add_contact(args, address_book))
+        elif command == "get":
+            print(vm.get_records(args, address_book))
+        elif command == "all":
+            print(vm.show_all(address_book))
+        elif command == "remove":
+            print(vm.remove_record(args, address_book))
+        elif command == "add-phone":
+            print(vm.add_phone(args, address_book))
+        elif command == "get-phone":
+            print(vm.get_phones(args, address_book))
+        elif command == "change-phone":
+            print(vm.change_phone(args, address_book))
+        elif command == "remove-phone":
+            print(vm.remove_phone(args, address_book))
+        elif command == "set-birthday":
+            print(vm.set_birthday(args, address_book))
+        elif command == "get-birthday":
+            print(vm.show_birthday(args, address_book))
+        elif command == "remove-birthday":
+            print(vm.remove_birthday(args, address_book))
+        elif command == "birthdays":
+            print(vm.birthdays(args, address_book))
+        elif command == "set-email":
+            print(vm.set_email(args, address_book))
+        elif command == "get-email":
+            print(vm.show_email(args, address_book))
+        elif command == "remove-email":
+            print(vm.remove_email(args, address_book))
+        elif command == "set-address":
+            print(vm.set_address(args, address_book))
+        elif command == "get-address":
+            print(vm.show_address(args, address_book))
+        elif command == "remove-address":
+            print(vm.remove_address(args, address_book))
         else:
             print("Invalid command.")
 
